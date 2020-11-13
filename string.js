374afea1912566b109946ab6b09fa9c2b26fa202
jiyt <15651300201@wo.cn>
2020-11-13 09:42:30 +0800

    字符串

---
+++ string.js
@@ -0,0 +1,20 @@
+var str = 'hello'
+
+// charCodeAt 访问字符串对应下表的ASCII码值
+// str.charCodeAt(1)
+
+// fromCharCode 将传入的ASCII码值返回字符
+// String.fromCharCode(97, 98, 99)
+
+// indexOf(subStr, start) lastIndexOf(subStr) 查找
+// str.indexOf('l', 2)
+// str.lastIndexOf('l')
+
+// search(subStr) 查找，可以传正则表达式
+// str.search(/e/)
+
+// atob base64解码 btoa base64编码
+
+
+
+
